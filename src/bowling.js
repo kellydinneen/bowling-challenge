c58ff6738ed99f879d4d9252ed9b04a88c1e243e
kellydinneen <kgdinneen@gmail.com>
2020-12-11 11:01:26 -0700

    Solve challenge

--- src/bowling.js
+++ src/bowling.js
@@ -12,6 +12,42 @@
 // SPARE:  10 + number of pins knocked down in next roll
 
 function totalScore(input) {
-}
+  const characters = input.split('');
+  const roll = characters.filter(digit => digit !== ' ' && digit !== ',');
+
+  const rollScores = roll.map(roll => {
+    if (roll != 'X' && roll != '/') {
+      return Number(roll);
+    } else {
+      return roll;
+    }
+  });
+  console.log(rollScores);
+
+  let rollTotal = 0;
+  for (var i = rollScores.length - 1; i >= 0; i--) {
+    if (rollScores[i] === 'X') {
+      rollTotal += 10 + getSimpleScoreEquivalent(rollScores, i+1) + getSimpleScoreEquivalent(rollScores, i+2);
+    } else if (rollScores[i] === '/') {
+      rollTotal += 10 - rollScores[i - 1] + getSimpleScoreEquivalent(rollScores, i+1);
+    } else {
+      rollTotal += rollScores[i];
+    }
+  };
+
+  return rollTotal;
+};
+
+function getSimpleScoreEquivalent(array, index) {
+  if (array[index] === 'X') {
+    return 10;
+  } else if (array[index] === '/') {
+    return 10 - array[index - 1];
+  } else if (array[index] === undefined) {
+    return 0;
+  } else {
+    return array[index];
+  }
+};
 
 module.exports = totalScore;
--- test/bowling-test.js
+++ test/bowling-test.js
@@ -7,25 +7,25 @@ describe('totalScore', () => {
   it('scores game with no strikes or spares', () => {
     const scores = '23  23  23  23  23  23  23  23  23  23';
     // frames:  5,  5,  5,  5,  5,  5,  5,  5,  5,  5
-    // total:   5, 10, 15, 20, 25, 30, 35, 40, 45, 50    
+    // total:   5, 10, 15, 20, 25, 30, 35, 40, 45, 50
     expect(totalScore(scores)).to.equal(50);
   });
 
-  it.skip('scores game with spares', () => {
+  it('scores game with spares', () => {
     const scores = '2/  32  23  23  23  23  23  23  23  23';
     // frames: 13,  5,  5,  5,  5,  5,  5,  5,  5,  5
     // total:  13, 18, 23, 28, 33, 38, 43, 48, 53, 58
     expect(totalScore(scores)).to.equal(58);
   });
 
-  it.skip('scores game with strikes', () => {
+  it('scores game with strikes', () => {
     const scores =  'X  23  23  23  23  23  23  23  23  23';
     // frames: 15,  5,  5,  5,  5,  5,  5,  5,  5,  5
     // total:  15, 20, 25, 30, 35, 40, 45, 50, 55, 60
     expect(totalScore(scores)).to.equal(60);
   });
 
-  it.skip('scores game with consecutive strikes', () => {
+  it('scores game with consecutive strikes', () => {
     // get next 2 rolls so first one is 10 + 10 + 10, 2nd one is 10 + 10 + 2, 3rd one is 10 + 2 + 3
     const scores =  'X   X   X  23  23  23  23  23  23  23';
     // frames: 30, 22, 15,  5,  5,  5,  5,  5,  5,  5
@@ -33,7 +33,7 @@ describe('totalScore', () => {
     expect(totalScore(scores)).to.equal(102);
   });
 
-  it.skip('scores game with spare after strike', () => {
+  it('scores game with spare after strike', () => {
     const scores =  'X  2/  23  23  23  23  23  23  23  23';
     // frames: 20, 12,  5,  5,  5,  5,  5,  5,  5,  5
     // total:  20, 32, 37, 42, 47, 52, 57, 62, 67, 72
